65db288c25fbf0ff0bbd5f3ffb8a755274a80059
Federica <148449872+feffyx@users.noreply.github.com>
2025-09-22 16:22:54 +0200

    Initial Commit

---
+++ Ephemeral/EphemeralApp.swift
@@ -0,0 +1,17 @@
+//
+//  EphemeralApp.swift
+//  Ephemeral
+//
+//  Created by Federica Ziaco on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EphemeralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
